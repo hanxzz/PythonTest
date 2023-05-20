672074fa8cc7db0f3f28b7f5cf73aadfce608d5e
Han <43496337+hanxzz@users.noreply.github.com>
2023-05-20 20:16:51 +0530

    Update and rename keypressEventsTurtle.py to EventsTurtle.py

--- turtleTest/EventsTurtle.py
+++ turtleTest/EventsTurtle.py
@@ -12,11 +12,19 @@ def turn_left():
   t.left(90)
 def turn_right():
   t.right(90)
+def gotoPosition(x,y):
+  print("You clicked at {0},{1}".format(x,y))
+  t.goto(x,y)
+def spin():
+  for i in range(4):
+    t.right(90)
 def exit():
   wn.bye()
 wn.onkey(move, "space")
-wn.onkey(turn_left, "Left")
+wn.onkey(turn_left, "Left")     #Keypress events
 wn.onkey(turn_right, "Right")
+wn.onkey(spin, "s")
+wn.onclick(gotoPosition)        #mouse events
 wn.onkey(exit, "q")
 wn.listen()
 wn.mainloop()
